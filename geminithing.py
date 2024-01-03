a11dbf5f08e35cbfe1af3a0e4a22060403be0308
Antronit <155570331+Antronit@users.noreply.github.com>
2024-01-03 17:32:16 +0000

    fixed it

--- geminithing.py
+++ geminithing.py
@@ -18,14 +18,14 @@ def to_markdown(text):
   text = text.replace('•', '  *')
   return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
 
-print('What is your API key\n')
-input(apikey)
+
+apikey=input('What is your API key\n')
 genai.configure(api_key=apikey)
 
 chat = model.start_chat(history=[])
 chat
-print('What do you want to ask Gemini Pro')
-input(asked)
+print()
+asked=input('What do you want to ask Gemini Pro')
 chat.send_message(asked)
 response = chat.send_message(asked)
 to_markdown(response.text)
